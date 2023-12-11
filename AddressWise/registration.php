17081ec410b1a874ef849f0afad5af0ddd47b73a
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-12-11 13:21:50 +0530

    AddressWise Extension

--- AddressWise/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AddressWiseExtension_AddressWise',
-    __DIR__
-);
