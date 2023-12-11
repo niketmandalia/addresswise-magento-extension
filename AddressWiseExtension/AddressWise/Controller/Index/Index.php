324fcbdbfadfb8f771be8e2c0ceef4d37f70bbf7
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-12-11 13:12:40 +0530

    AddressWise Extension

---
+++ AddressWiseExtension/AddressWise/Controller/Index/Index.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AddressWiseExtension\AddressWise\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute the controller action.
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
+    }
+}
